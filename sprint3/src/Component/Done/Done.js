513e44cf9e4d99553a56b32dc768d6484c75c515
Olivialamyc <lamemily0209@gmail.com>
2021-09-08 17:43:52 +0800

    update component  done

--- sprint3/src/Component/Done/Done.js
+++ sprint3/src/Component/Done/Done.js
@@ -1,97 +1,70 @@
-import React, { Component } from "react";
-import { useState, useEffect } from "react";
 import "./Done.css";
-import TaskCard from "../../TaskCard";
 import App from "../../App";
+import React, { useState, useEffect } from "react";
+import TaskCard from "../../TaskCard";
 
-class DoneCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projects: [],
-    };
-  }
-
-  componentDidMount() {
-    const getOneTask = async (id) => {
-      const url = `http://localhost:8080/tasks/${id}`;
+export default function Done() {
+  const [tasks, setTasks] = useState([]);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [showTaskModal, setShowTaskModal] = useState(false);
+  useEffect(() => {
+    const getOneTask = async () => {
+      const url = `http://localhost:8080/tasks`;
       const res = await fetch(url);
       const task = await res.json();
-      return task;
+      setTasks(task);
     };
-    const fillModal = async (id) => {
-      const task = await getOneTask(id);
-      setModalTitle(task.title);
-      setModalDueDate(task.due);
-      setModalDescription(task.description);
-      setModalAssignment(task.assignment);
-      setModalStatus(task.status);
-      setModalPriority(task.priority);
+    getOneTask();
+    return () => {
+      // cleanup
     };
+  }, []);
+  useEffect(() => {
     const toggleTaskModal = () => {
       setShowTaskModal((curState) => !curState);
       setShowOverlay((curState) => !curState);
     };
+  }),
+    [];
 
+  useEffect(() => {
     const toggleDeleteModal = () => {
       setShowDeleteModal((curState) => !curState);
       setShowOverlay((curState) => !curState);
     };
-  }
-
-  render() {
-    const [tasks, setTasks] = useState([]);
-    const [showTaskModal, setShowTaskModal] = useState(false);
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
-    const [showOverlay, setShowOverlay] = useState(false);
-    const [taskID, setTaskID] = useState();
-    const [modalHeader, setModalHeader] = useState("Add New Task");
-    const [modalTitle, setModalTitle] = useState(null);
-    const [modalDueDate, setModalDueDate] = useState(null);
-    const [modalDescription, setModalDescription] = useState(null);
-    const [modalAssignment, setModalAssignment] = useState(null);
-    const [modalStatus, setModalStatus] = useState(null);
-    const [modalPriority, setModalPriority] = useState(null);
-    const [blockEditButton, setBlockEditButton] = useState(true);
-    const [blockAddButton, setBlockAddButton] = useState(true);
-    const [submitMethod, setSubmitMethod] = useState("post");
-    const projects = this.state.projects.map((project) => {
-      return {
-        <TaskCard
-          id={t._id}
-          title={t.title}
-          due={t.due}
-          assignment={t.assignment}
-          description={t.description}
-          status={t.status}
-          priority={t.priority}
-          key={index}
-          onClickEditBtn={async () => {
-            await fillModal(t._id);
-            setModalHeader("Edit Task");
-            setBlockAddButton(true);
-            setBlockEditButton(false);
-            setTaskID(t._id);
-            toggleTaskModal();
-          }}
-          onClickDeleteBtn={() => {
-            setTaskID(t._id);
-            toggleDeleteModal();
-          }}
-        />
-        };
-    });
-
-    if(tasks.status==='done'){
-      return {project}
-    }
-  }
-    
-  
+  }, []);
 
   return (
     <div>
-      <section className="task-card-container">{projects}</section>
+      {tasks
+        .filter((t) => t.status === "done")
+        .map((t) => {
+          return (
+            <TaskCard
+              id={t._id}
+              title={t.title}
+              due={t.due}
+              assignment={t.assignment}
+              description={t.description}
+              status={t.status}
+              priority={t.priority}
+              key={t._id}
+              // onClickEditBtn={async () => {
+              //   await fillModal(t._id);
+              //   setModalHeader("Edit Task");
+              //   setBlockAddButton(true);
+              //   setBlockEditButton(false);
+              //   setTaskID(t._id);
+              //   toggleTaskModal();
+              // }}
+              // onClickDeleteBtn={() => {
+              //   setTaskID(t._id);
+              //   toggleDeleteModal();
+              // }}
+            />
+          );
+        })}
     </div>
   );
 }
